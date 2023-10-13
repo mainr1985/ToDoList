a514d7025e1e254a8f873edc6d2643baae0e32b5
Maíra N. Rocha <mairanrocha@hotmail.com>
2023-10-13 18:16:18 -0300

    Criada lista de tarefas e adicionada validação de credenciais para acesso

---
+++ todolist/src/main/java/br/com/profdanieleleao/todolist/filter/FilterTaskAuth.java
@@ -0,0 +1,54 @@
+package br.com.profdanieleleao.todolist.filter;
+
+import java.io.IOException;
+import java.util.Base64;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import br.com.profdanieleleao.todolist.user.IUserRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component //sem isso não é implementado o Filter. Component é a classe mais genérica de gerenciamento dentro do spring.
+//uma requisição ao ser feita passa primeiro aqui, depois vai pro controller
+public class FilterTaskAuth extends OncePerRequestFilter{
+	@Autowired
+	private IUserRepository userRepository; //NÃO PODE DECLARAR DENTRO DO doFilterInternal, TEM QUE SER AQUI.
+	
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+		
+		//1º passo: pegar a autenticação passada pelo usuário (usu + senha)
+		var authorization = request.getHeader("Authorization"); //vem na aba 'Auth' do postman/api dog e é semelhante a um header 
+		System.out.println("Authorization " + authorization);
+		
+		var authEncoded = authorization.substring("Basic".length()).trim(); //trim tira os espaços
+		byte[] authDecode = Base64.getDecoder().decode(authEncoded); // authorization vem criptografado em base 64, precisa decodificar.
+		var authString = new String (authDecode); 
+		String[] credentials = authString.split(":"); //divide onde tem ":" pra separar nome de usuário e senha --> [danieleleao,12345]
+		String username = credentials[0];
+		String password = credentials[1];
+		
+		//2º passo: validar se usuário existe
+		var user = this.userRepository.findByUsername(username);
+		if (user == null) {
+			response.sendError(401); //erro 401 -> unauthorized
+		}
+		else {
+			//3º passo: validar se senha existe
+			var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword()); //porque o método findByUsername retorna um objeto do tipo UserModel, então traz a senha também.
+			if (passwordVerify.verified) { //método boolean do Bcrypt  
+				filterChain.doFilter(request, response);
+			}			
+			else {
+				response.sendError(401);
+			}
+		}
+	}
+}
--- todolist/src/main/java/br/com/profdanieleleao/todolist/task/TaskModel.java
+++ todolist/src/main/java/br/com/profdanieleleao/todolist/task/TaskModel.java
@@ -3,7 +3,12 @@ package br.com.profdanieleleao.todolist.task;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 /*
@@ -15,16 +20,26 @@ import lombok.Data;
  * Data de início 
  * Data de término
  *  Prioridade
+ *  Id do usuário
  * */
 
 @Data
-@Entity(name="tb_task")
+@Entity(name="tb_tasks")
 public class TaskModel {
+	
+	@Id
+	@GeneratedValue(generator = "UUID")
 	private UUID id;
 	private String description;
+	
+	@Column(length=50) //limita a 50 caracteres o campo
 	private String title;
 	private LocalDateTime startAt;
 	private LocalDateTime endAt;
 	private String priority;	
+	private UUID idUser;
+	
+	@CreationTimestamp
 	private LocalDateTime createdAt;
+	
 }
