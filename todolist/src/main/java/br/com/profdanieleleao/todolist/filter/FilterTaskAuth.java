f708fe950699ef6dc13b7f2a2a088ece35efd970
Maíra N. Rocha <mairanrocha@hotmail.com>
2023-10-14 21:47:49 -0300

    Adicionadas validações e iniciado crud - update está com problema, rever

--- todolist/src/main/java/br/com/profdanieleleao/todolist/filter/FilterTaskAuth.java
+++ todolist/src/main/java/br/com/profdanieleleao/todolist/filter/FilterTaskAuth.java
@@ -24,31 +24,40 @@ public class FilterTaskAuth extends OncePerRequestFilter{
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
 		
-		//1º passo: pegar a autenticação passada pelo usuário (usu + senha)
-		var authorization = request.getHeader("Authorization"); //vem na aba 'Auth' do postman/api dog e é semelhante a um header 
-		System.out.println("Authorization " + authorization);
-		
-		var authEncoded = authorization.substring("Basic".length()).trim(); //trim tira os espaços
-		byte[] authDecode = Base64.getDecoder().decode(authEncoded); // authorization vem criptografado em base 64, precisa decodificar.
-		var authString = new String (authDecode); 
-		String[] credentials = authString.split(":"); //divide onde tem ":" pra separar nome de usuário e senha --> [danieleleao,12345]
-		String username = credentials[0];
-		String password = credentials[1];
-		
-		//2º passo: validar se usuário existe
-		var user = this.userRepository.findByUsername(username);
-		if (user == null) {
-			response.sendError(401); //erro 401 -> unauthorized
-		}
-		else {
-			//3º passo: validar se senha existe
-			var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword()); //porque o método findByUsername retorna um objeto do tipo UserModel, então traz a senha também.
-			if (passwordVerify.verified) { //método boolean do Bcrypt  
-				filterChain.doFilter(request, response);
-			}			
-			else {
-				response.sendError(401);
+		//identificando qual a rota que está sendo acessada, porque só importa /tasks/
+		var servletPath = request.getServletPath();
+		if (servletPath.startsWith("/tasks/")) {
+			//1º passo: pegar a autenticação passada pelo usuário (usu + senha)
+			var authorization = request.getHeader("Authorization"); //vem na aba 'Auth' do postman/api dog e é semelhante a um header 
+						
+			var authEncoded = authorization.substring("Basic".length()).trim(); //trim tira os espaços
+			byte[] authDecode = Base64.getDecoder().decode(authEncoded); // authorization vem criptografado em base 64, precisa decodificar.
+			var authString = new String (authDecode); 
+			
+			String[] credentials = authString.split(":"); //divide onde tem ":" pra separar nome de usuário e senha --> [danieleleao,12345]
+			String username = credentials[0];
+			String password = credentials[1];
+			
+			//2º passo: validar se usuário existe
+			var user = this.userRepository.findByUsername(username);
+			if (user == null) {
+				response.sendError(401); //erro 401 -> unauthorized
 			}
+			else {
+				//3º passo: validar se senha existe
+				var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword()); //porque o método findByUsername retorna um objeto do tipo UserModel, então traz a senha também.
+				if (passwordVerify.verified) { //método boolean do Bcrypt  
+					//ensinando o método a pegar o id do usuário logado pra não precisar que o usuário informe
+					request.setAttribute("idUser",user.getId());
+					filterChain.doFilter(request, response);
+				}			
+				else {
+					response.sendError(401);
+				}
+			}
+		}	
+		else {
+			filterChain.doFilter(request, response); //se não for da rota /tasks/, segue em frente.
 		}
-	}
+		}		
 }
--- todolist/src/main/java/br/com/profdanieleleao/todolist/task/TaskController.java
+++ todolist/src/main/java/br/com/profdanieleleao/todolist/task/TaskController.java
@@ -1,10 +1,20 @@
 package br.com.profdanieleleao.todolist.task;
 
+import java.util.List;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/tasks")
@@ -13,9 +23,40 @@ public class TaskController {
 	@Autowired
 	private ITaskRepository taskRepository;
 	
-	@PostMapping("/")
-	public TaskModel create (@RequestBody TaskModel taskModel) {
+	@PostMapping("/") 
+	//HttpServletRequest é passado como parâmetro porque é ele que sabe a id do usuário
+	public ResponseEntity create (@RequestBody TaskModel taskModel, HttpServletRequest request) {
+		var idUser = request.getAttribute("idUser");
+
+		//adicionando validação de horas/data
+		//var currentDate = LocalDateTime.now();
+		/*if (currentDate.isAfter(taskModel.getStartAt()) || currentDate.isAfter(taskModel.getEndAt())) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Data de início/término inválida");
+		}*/
+		/*if(taskModel.getStartAt().isAfter(taskModel.getEndAt())) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Data de início não pode ser maior que a data de término");
+		}*/
+			
+		taskModel.setIdUser((UUID) idUser); //fazendo cast pra transformar o valor recebido em UUID.
 		var task = this.taskRepository.save(taskModel);
-		return task;
+		return ResponseEntity.status(HttpStatus.OK).body(task);
 	}
+	
+	//método para listar as tarefas salvas pelo usuário
+	@GetMapping("/") //pra trazer tudo que seja relacionado ao usuário
+	public List<TaskModel> list(HttpServletRequest request) {
+		var idUser = request.getAttribute("idUser"); //pegando o id 
+		var tasks = this.taskRepository.findByIdUser((UUID) idUser);
+		return tasks;
+	}
+	
+	//método para fazer update nas tarefas
+	//http://localhost:8080/tasks/id-da-tarefa
+	@PutMapping("/{id}")
+	public TaskModel update (@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id) {
+		var idUser = request.getAttribute("idUser"); //pegando o id 
+		taskModel.setIdUser((UUID) idUser);
+		taskModel.setId(id);		
+		return this.taskRepository.save(taskModel);
+	}	
 }
